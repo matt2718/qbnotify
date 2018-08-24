56e932ea7c3e5e3982efe6e1091b8e0f9759c0bb
Matt Mitchell <mattmitchell2718@gmail.com>
2018-08-23 23:08:28 -0600

    Misc cleanup

--- static/mapsetup.js
+++ static/mapsetup.js
@@ -1,8 +1,8 @@
 colorDefaults = {
-    strokeColor: '#0052bb',
-    strokeOpacity: 0.8,
+    strokeColor: '#0000ff',
+    strokeOpacity: 0.7,
     strokeWeight: 1,
-    fillColor: '#0052bb',
+    fillColor: '#0000ff',
     fillOpacity: 0.3
 };
 
@@ -13,41 +13,43 @@ function initMap() {
         mapTypeId: 'roadmap'
     });
 
+    // state notifications
     [].forEach.call(
-	document.getElementsByClassName('notify-state'),
-	function(elem) { drawState(elem.dataset.state); }
+      document.getElementsByClassName('notify-state'),
+      function(elem) { drawState(elem.dataset.state); }
     );
 
+    // circular notifications
     [].forEach.call(
-	document.getElementsByClassName('notify-circle'),
-	function(elem) {
-	    var radius = parseFloat(elem.dataset.radius);
-	    // google maps accepts radius in meters, we need to convert
-	    switch(elem.dataset.unit) {
-	    case 'mi':
-		radius *= 1609.3;
-		break;
-	    case 'ft':
-		radius *= 0.3048;
-		break;
-	    case 'km':
-		radius *= 1000.0;
-		break;
-	    default:
-		radius *= 1.0;
-	    }
-	    
-	    drawCircle(
-		parseFloat(elem.dataset.lat),
-		parseFloat(elem.dataset.lng),
-		radius
-	    );
-	}
+      document.getElementsByClassName('notify-circle'),
+      function(elem) {
+          var radius = parseFloat(elem.dataset.radius);
+          // google maps accepts radius in meters, we need to convert
+          switch(elem.dataset.unit) {
+          case 'mi':
+            radius *= 1609.3;
+            break;
+          case 'ft':
+            radius *= 0.3048;
+            break;
+          case 'km':
+            radius *= 1000.0;
+            break;
+          default:
+            radius *= 1.0;
+          }
+          
+          drawCircle(
+            parseFloat(elem.dataset.lat),
+            parseFloat(elem.dataset.lng),
+            radius
+          );
+      }
     );
 }
 
+// JSON XHR wrapper for convenience
 function getJSON(url, callback) {
-    // JSON XHR wrapper for convenience
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
@@ -60,15 +62,9 @@ function getJSON(url, callback) {
     xhr.send();
 };
 
-function drawPoly(coords) {
-    var params = Object.assign({paths: coords}, colorDefaults);
-    var polygon = new google.maps.Polygon(params);
-    polygon.setMap(map);
-}
-
 function drawCircle(lat, lng, radius) {
     var params = Object.assign({
-	center: {lat: lat, lng: lng},
+      center: {lat: lat, lng: lng},
         radius: radius
     }, colorDefaults);
     var circle = new google.maps.Circle(params);
@@ -77,5 +73,10 @@ function drawCircle(lat, lng, radius) {
 
 function drawState(abbr) {
     getJSON(document.location.origin + '/static/polygons/' + abbr + '.json',
-	    drawPoly);
+            function(coords) {
+                // draw polygon from coordinates
+                var params = Object.assign({paths: coords}, colorDefaults);
+                var polygon = new google.maps.Polygon(params);
+                polygon.setMap(map);
+            });
 }
